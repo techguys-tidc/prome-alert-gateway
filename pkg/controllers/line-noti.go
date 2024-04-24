8157a8f945f6237422628531793d79de73eac270
Jutinant Mah <jutinant@gmail.com>
2024-04-24 16:23:58 +0700

    add emoji for line

--- pkg/controllers/line-noti.go
+++ pkg/controllers/line-noti.go
@@ -20,6 +20,11 @@ func LineNoti(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"code": 0})
 	for _, alert := range payload.Alerts {
+		if payload.Status == "firing" {
+			payload.Status = "🔥" + payload.Status + "🔥"
+		} else if payload.Status == "resolved" {
+			payload.Status = "👌" + payload.Status + "👌"
+		}
 		go func(alert models.Alerts) {
 			message := fmt.Sprintf("\nStatus: %s\nAlertname: %s\nSummary: %s\nDesc: %s",
 				payload.Status, alert.Labels.AlertName, alert.Annotations.Summary, alert.Annotations.Description)
