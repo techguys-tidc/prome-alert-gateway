912228bbcf3816881b35ea3e95995c0494921416
chanathip-tor <chanathipa.tor@gmail.com>
2025-02-04 23:00:14 +0700

    fix curl https > http

--- curl.sh
+++ curl.sh
@@ -1,3 +1,3 @@
-curl -k -X POST "https://localhost:8080/uvdesk-notify" \
+curl -k -X POST "http://localhost:8080/uvdesk-notify" \
      -H "Content-Type: application/json" \
      -d @example-request-UVdesk.json
