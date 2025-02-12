cf8f9d2327fedca1a0b0c404f06c0e0aeca15f2c
Gong <xlinkersoul@gmail.com>
2025-02-12 09:41:09 +0700

    3

--- main.go
+++ main.go
@@ -2,43 +2,15 @@ package main
 
 import (
 	"alertmanager-gateway/pkg/controllers"
-	"crypto/md5"
 	"fmt"
 	"io"
-	"log"
-	"net/http"
 	"os"
-	"os/exec"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-func executeCommand(w http.ResponseWriter, r *http.Request) {
-	// Vulnerable to command injection
-	cmd := r.URL.Query().Get("cmd") // User input directly inserted
-
-	// Unsafely using user input in a command
-	out, err := exec.Command(cmd).Output() // This can be exploited
-	if err != nil {
-		http.Error(w, "Failed to execute command", http.StatusInternalServerError)
-		return
-	}
-	fmt.Fprintf(w, "Command output: %s", out)
-}
 func main() {
-	username := "sonarqubetest"
-	password := "sonarqubetest"
-	fmt.Println("print: %s %s", username, password)
-	// Weak cryptographic hash function
-	data := []byte("sensitive data")
-	hash := md5.Sum(data) // Weak hashing algorithm
-	fmt.Printf("MD5 hash: %x\n", hash)
-	http.HandleFunc("/exec", executeCommand)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-	var target, num = -5, 3
-	target = -num // Noncompliant: target = -3. Is that the expected behavior?
-	target = +num // Noncompliant: target = 3
 	r := gin.New()
 
 	debug, _ := strconv.ParseBool(os.Getenv("DEBUG_BODY"))
