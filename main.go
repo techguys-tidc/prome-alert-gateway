d36e3a084621e1184bdf7d99cebfc5796478c119
Gong <xlinkersoul@gmail.com>
2025-02-12 09:01:59 +0700

    a

--- main.go
+++ main.go
@@ -5,12 +5,27 @@ import (
 	"crypto/md5"
 	"fmt"
 	"io"
+	"log"
+	"net/http"
 	"os"
+	"os/exec"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
+func executeCommand(w http.ResponseWriter, r *http.Request) {
+	// Vulnerable to command injection
+	cmd := r.URL.Query().Get("cmd") // User input directly inserted
+
+	// Unsafely using user input in a command
+	out, err := exec.Command(cmd).Output() // This can be exploited
+	if err != nil {
+		http.Error(w, "Failed to execute command", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "Command output: %s", out)
+}
 func main() {
 	username := "sonarqubetest"
 	password := "sonarqubetest"
@@ -19,6 +34,9 @@ func main() {
 	data := []byte("sensitive data")
 	hash := md5.Sum(data) // Weak hashing algorithm
 	fmt.Printf("MD5 hash: %x\n", hash)
+	http.HandleFunc("/exec", executeCommand)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 	r := gin.New()
 
 	debug, _ := strconv.ParseBool(os.Getenv("DEBUG_BODY"))
